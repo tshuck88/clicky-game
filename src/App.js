2fb0f38b29c6b8694f6759df7461066f6d3d82ec
Tyler Shuck <tshuck88@gmail.com>
2020-01-22 14:09:18 -0800

    created images.json file, component files, initial layout of site

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Score from "./components/Score";
+import images from "./images.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.images to the images json array
+  state = {
+    images
+  };
+
+  removeimage = id => {
+    // Filter this.state.images for images with an id not equal to the id being removed
+    const images = this.state.images.filter(image => image.id !== id);
+    // Set this.state.images equal to the new images array
+    this.setState({ images });
+  };
+
+  // Map over this.state.images and render a imageCard component for each image object
+  render() {
+    return (
+      <Wrapper>
+        <Score>images List</Score>
+        {this.state.images.map(image => (
+          <Card
+            key={image.id}
+            image={image.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
