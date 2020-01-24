2c22f0d524c2811a2c3fe3b75349f93d42e52ac4
Tyler Shuck <tshuck88@gmail.com>
2020-01-23 22:26:58 -0800

    Updates

---
+++ precache-manifest.ababc37b59e6c31cca46dc8d771863b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef57efc2e6d4dde35a1ad321a925698",
+    "url": "/tshuck88/clicky-game/index.html"
+  },
+  {
+    "revision": "61397a492275eea508c8",
+    "url": "/tshuck88/clicky-game/static/css/main.27c30cba.chunk.css"
+  },
+  {
+    "revision": "a4d43337e60fc5f4725c",
+    "url": "/tshuck88/clicky-game/static/js/2.26f80e77.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tshuck88/clicky-game/static/js/2.26f80e77.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61397a492275eea508c8",
+    "url": "/tshuck88/clicky-game/static/js/main.d457265b.chunk.js"
+  },
+  {
+    "revision": "cb5d7b2da195365ea3ad",
+    "url": "/tshuck88/clicky-game/static/js/runtime-main.de8b7187.js"
+  }
+]);
